7b3e8950382603f9e97f3acb8044166acdc1ef62
gabrieleguido <guido.2144286@studenti.uniroma1.it>
2026-04-12 12:55:49 +0200

    test_crawler_wiki

---
+++ crawler_test.py
@@ -0,0 +1,29 @@
+import asyncio 
+from crawl4ai import AsyncWebCrawler,BrowserConfig,CrawlerRunConfig,CacheMode
+
+async def main():
+
+    browser_cfg = BrowserConfig(headless=False) 
+
+    crawler_cfg = CrawlerRunConfig(
+        cache_mode=CacheMode.BYPASS,
+        word_count_threshold=20,
+        
+        # css_selector="#mw-content-text",
+        # excluded_selector=".mw-editsection, .reflist, .navbox, .infobox," \
+        # "#Notes, #See_also, #External_links, #References, #Citations" \
+        # ".navbar, .stub, .plainlinks, .metadata, " \
+        # ".vertical-navbox, .printfooter"
+        target_elements=["h1","h2","h3","p"]
+        ) 
+
+    async with AsyncWebCrawler(config=browser_cfg) as crawler:
+        result = await crawler.arun(
+            url = "https://en.wikipedia.org/wiki/Peroni_(company)",
+            config= crawler_cfg
+        )
+
+        file = open("crawler_result_test.txt","w")
+        file.write(result.markdown)
+        file.close()
+asyncio.run(main())
