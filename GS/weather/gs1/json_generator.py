d4d622b8dd52eecc391878bfbebba5892aca7716
Nicolò Carbone <ncarbone2004@gmail.com>
2026-04-14 09:51:53 +0200

    gs 1 completo weather

--- GS/weather/gs1/json_generator.py
+++ GS/weather/gs1/json_generator.py
@@ -2,21 +2,21 @@ import json
 
 # Usa il context manager 'with' per gestire l'apertura/chiusura
 try:
-    with open("anteprima-liverpool-paris-saint-germain-champions-league.html", "r", encoding="utf-8") as html_file:
+    with open("gs1_html.html", "r", encoding="utf-8") as html_file:
         html_text = html_file.read()
     
-    with open("anteprima-liverpool-paris-saint-germain-champions-league.txt", "r", encoding="utf-8") as golden_file:
+    with open("gs1.txt", "r", encoding="utf-8") as golden_file:
         golden_text = golden_file.read() 
 
     json_entry = {
-        "url": "https://it.uefa.com/uefachampionsleague/news/02a4-2060af553568-cd2fcc38c28e-1000--anteprima-liverpool-paris-saint-germain-champions-league",
-        "domain": "it.uefa.com",
-        "title": "anteprima-liverpool-paris-saint-germain-champions-league",
+        "url": "https://weather.com/it-IT/tempo/oggi/l/104b5c3a7e17868e40f84026b44fd565a02ee18193bb030a5cbd3076e58c01bc",
+        "domain": "www.weather.com",
+        "title": "Il meteo di oggi a Roma, città metropolitana di Roma Capitale",
         "html_text": html_text,
         "gold_text": golden_text
     }
 
-    with open("anteprima-liverpool-paris-saint-germain-champions-league.json", "w", encoding="utf-8") as result:
+    with open("gs1.json", "w", encoding="utf-8") as result:
         # Usa indent per rendere il JSON leggibile, altrimenti è un muro di testo inutile
         json.dump(json_entry, result, indent=4, ensure_ascii=False)
         
