28a1f9c6891e06a62d3de83bee1b1c8cea5c8b6f
gabrieleguido <guido.2144286@studenti.uniroma1.it>
2026-04-14 10:13:14 +0200

    parser_weather

--- parse_cleaner.py
+++ parse_cleaner.py
@@ -3,19 +3,23 @@ import re
 
 class ParseCleaner(BaseModel):
     @staticmethod
-    def parsed_clean(file_name_src:str,file_name_dst:str,enc:str)->None:
+    def parsed_clean(file_name_src:str,file_name_dst:str,enc:str,link_del_flag:bool=True)->None:
         """
             Scrive nel file di output il testo markdown preso in input pulito
             Argomenti:
                 file_name_src(str):Nome file del markdown da pulire
                 file_name_dst(str):Nome file destinazione del testo pulito
                 enc(str):Encoding dei file 
+                link_del_flag(bool): True per eliminare i link
         """
         markdown_file = open(file_name_src,"r",encoding=enc)
         clean_file = open(file_name_dst,"w",encoding=enc)
         for line in markdown_file:
-            line = re.sub(r'\(\s*https?://[^)]*\)',' ',line)
-            line = re.sub(r'\[\[\d+\]\]',' ',line)
+            #link delete: line = re.sub(r'\(\s*https?://[^)]*\)',' ',line)
+            if(link_del_flag):
+                #elimino le note []
+                line = re.sub(r'\[\[\d+\]\]',' ',line)
+
             line = re.sub(r'[^a-zA-Z0-9]',' ',line)
             line = line.split(" ")
             for w in line:
