984e77dc3c56fe14fd4c65fc83bbf936aa8c9d85
Nicolò Carbone <ncarbone2004@gmail.com>
2026-04-14 10:24:53 +0200

    gs 3 completo weather

--- GS/weather/gs3/json_generator.py
+++ GS/weather/gs3/json_generator.py
@@ -2,21 +2,21 @@ import json
 
 # Usa il context manager 'with' per gestire l'apertura/chiusura
 try:
-    with open("gs1_html.html", "r", encoding="utf-8") as html_file:
+    with open("gs3_html.html", "r", encoding="utf-8") as html_file:
         html_text = html_file.read()
     
-    with open("gs1.txt", "r", encoding="utf-8") as golden_file:
+    with open("gs3.txt", "r", encoding="utf-8") as golden_file:
         golden_text = golden_file.read() 
 
     json_entry = {
-        "url": "",
-        "domain": "",
-        "title": "",
+        "url": "https://weather.com/it-IT/tempo/10giorni/l/104b5c3a7e17868e40f84026b44fd565a02ee18193bb030a5cbd3076e58c01bc",
+        "domain": "www.weather.com",
+        "title": "Previsioni meteo per 10 giorni- Roma, città metropolitana di Roma Capitale",
         "html_text": html_text,
         "gold_text": golden_text
     }
 
-    with open("gs1.json", "w", encoding="utf-8") as result:
+    with open("gs3.json", "w", encoding="utf-8") as result:
         # Usa indent per rendere il JSON leggibile, altrimenti è un muro di testo inutile
         json.dump(json_entry, result, indent=4, ensure_ascii=False)
         
