1625f73de96e7cb63fc1f88900ca69167edbad0e
Nicolò Carbone <ncarbone2004@gmail.com>
2026-04-17 10:24:07 +0200

    update server con get full_gold_standard

--- TESTING/server.py
+++ TESTING/server.py
@@ -4,13 +4,12 @@ import json
 from urllib.parse import urlparse
 from token_compare import TokenCompare
 import os
+from typing import List
 
 app = FastAPI()
 
 # Lista dei domini assegnati
 domains_list = TokenCompare.get_domain_list()
-for d in domains_list:
-    print(d)
 
 folder_map = {
     "en.wikipedia.org": "wikipedia",
@@ -21,14 +20,21 @@ folder_map = {
 
 # Modello di risposta per GET /domains
 class DomainsListModel(BaseModel):
-    domains: list[str]
+    domains: List[str]
 
+
+# Modello di risposta per GET /gold_standard
 class GoldStandardModel(BaseModel):
     url: str
     domain: str
     title: str
     html_text: str
     gold_text: str
+
+
+# Modello di risposta per GET /full_gold_standard
+class FullGoldStandardModel(BaseModel):
+    gold_standard: List[GoldStandardModel]
 
 
 
@@ -47,7 +53,7 @@ async def get_gold_standard(url: str)->GoldStandardModel:
     """
     Restituisce oggetto JSON contenente il gold standard del dominio in input
     """
-    
+
     try:
         domain = TokenCompare.get_domain_from_url(url)
     except Exception:
@@ -73,4 +79,28 @@ async def get_gold_standard(url: str)->GoldStandardModel:
             
         except json.JSONDecodeError:
             raise HTTPException(status_code=500, detail="File json corrrotto")
+        
+
+
+@app.get("/full_gold_standard", response_model=FullGoldStandardModel)
+async def get_full_gold_standard(domain:str)->FullGoldStandardModel:
+    """
+    Restituisce oggetto JSON contenente la lista degli elementi di un GS per un dominio specifico
+    """
     
+    if domain not in domains_list:
+        raise HTTPException(status_code=404, detail="Dominio non supportato")
+
+    file_name = folder_map[domain]    
+    file_path = f"gs_data/{file_name}_gs.json"
+
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=500, detail=f"File {file_path} non trovato")
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        try:
+            gs_list = json.load(f)
+            return {"gold_standard": gs_list}
+
+        except json.JSONDecodeError:
+            raise HTTPException(status_code=500, detail="File json corrrotto")
