fc60ad740b029c754f3483f45d7bb568bb27185e
Nicolò Carbone <ncarbone2004@gmail.com>
2026-04-17 17:31:40 +0200

    parser weather

--- PARSER/parser_weather.py
+++ PARSER/parser_weather.py
@@ -8,19 +8,18 @@ async def main():
 
     crawler_cfg = CrawlerRunConfig(
             cache_mode=CacheMode.BYPASS,
-            word_count_threshold=20,
-            target_elements=["h1","h2","h3","title","p"]
+            target_elements=["h1","h2","h3","title","p","span"]
         ) 
 
     async with AsyncWebCrawler(config=browser_cfg) as crawler:
         result = await crawler.arun(
-            url = "https://www.nbcnews.com/science/space/artemis-ii-astronauts-return-nasa-moon-mission-rcna273651",
+            url = "https://weather.com/it-IT/tempo/oggi/l/104b5c3a7e17868e40f84026b44fd565a02ee18193bb030a5cbd3076e58c01bc",
             config= crawler_cfg
         )
 
-        file = open("crawler_result_nbcnews.txt","w", encoding='utf-8')
+        file = open("crawler_result_weather.txt","w", encoding='utf-8')
         file.write(result.markdown)
         file.close()
-        ParseCleaner.parsed_clean("crawler_result_nbcnews.txt","clean_nbcnews.txt",'UTF-8')
+        ParseCleaner.parsed_clean("crawler_result_weather.txt","clean_weather.txt",'UTF-8')
 
 asyncio.run(main())
