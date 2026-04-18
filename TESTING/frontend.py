6be5cb55406fe56f704777f4f0ff705918473271
Nicolò Carbone <ncarbone2004@gmail.com>
2026-04-18 16:31:58 +0200

    aggiunta commenti a frontend

--- TESTING/frontend.py
+++ TESTING/frontend.py
@@ -15,17 +15,20 @@ backend_url = "http://127.0.0.1:8003"
 # funzione per web ui
 @app.get("/", response_class=HTMLResponse)
 def web_ui(request:Request, domain:str=None):
+    """
+    Gestisce la web ui
+    """
     
     domains_list = []
 
     try:
         url = f"{backend_url}/domains"
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(url) as response:   # si collega al backend su url, con with la connessione viene automaticamente chiusa alla fine del blocco
             if response.status == 200:
-                data = response.read().decode('utf-8')
-                data_json = json.loads(data)
+                data = response.read().decode('utf-8')  # legge il body della risposta HTTP del server (testo grezzo) e la legge con codifica utf-8
+                data_json = json.loads(data)    # trasforma la stringa di testo in un dizionario python
 
-                domains_list = data_json.get("domains", [])
+                domains_list = data_json.get("domains", []) # restituisce la chiave "domains", se non la trova restituisce lista vuota senza crashare
     except Exception as e:
         print(f"Errore di connessione al backend {e}")    
     
