1e8dc83cdc3a0285fd1322f48331b1e9506a2536
Gabriele Di Martino <gabrieledm85@gmail.com>
2026-05-09 12:52:07 +0200

    modifica populate

--- PROGETTO/mariabd_data/populate_db.py
+++ PROGETTO/mariabd_data/populate_db.py
@@ -11,6 +11,7 @@ conn = mariadb.connect(
 
 gs_files_names = ["nbcnews_gs.json","uefa_gs.json","weather_gs.json","wikipedia_gs.json"]
 #per ogni file in gs_data
+cur = conn.cursor()
 for file_name in gs_files_names:
 
     #apro il file
@@ -18,23 +19,19 @@ for file_name in gs_files_names:
         gs_list = json.load(file)
 
         #inserisco i dati per ogni elemento nella lista del json
+       
         for entry in gs_list:
-
-            #creo le query parametrizzate (con i '?')
-            query_web = "INSERT INTO web_resources VALUES (?,?,?,?)"
-            query_gs = "INSERT INTO gold_standard VALUES (?,?)"
-
-            with conn.cursor() as cursor:
-
-                url = entry.get("url")
-                domain = entry.get("domain")
-                title = entry.get("title")
-                html_text = entry.get("html_text")
-                gold_text = entry.get("gold_text")
-
-                cursor.execute(query_web,(url,domain,title,html_text))
-                cursor.execute(query_gs,(url,gold_text))
-                res = cursor.fetchall()
-
-            conn.commit() 
-            print(res)
+            try: 
+                cur.execute( 
+                            "INSERT IGNORE INTO web_resources (url, domain, title, html_text) VALUES (?, ?, ?, ?)",
+                            (entry.get("url"), entry.get("domain"), entry.get("title"), entry.get("html_text"))
+                        )
+                cur.execute(
+                            "INSERT IGNORE INTO gold_standard (url, gold_text) VALUES (?, ?)",
+                            (entry.get("url"), entry.get("gold_text"))
+                        )
+            except mariadb.Error as e:
+                print(f"Errore durante l'inserimento di {entry.get('url')}: {e}")
+                
+        conn.commit() 
+    cur.close()
