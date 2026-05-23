3dcccbaa2faad7f0f50e176d3477c464f05bdf1c
gabriele <guido.2144286@studenti.uniroma1.it>
2026-05-23 16:29:50 +0200

    modelli_ordinati

--- PROGETTO/backend/src/server.py
+++ PROGETTO/backend/src/server.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, HTTPException
 import requests
-from pydantic import BaseModel
 import json
 from urllib.parse import urlparse,unquote
 import urllib.request
+from modelli_pydantic import AddGoldStandardInputModel, AddOutputModel, AddWebResourceInputModel, DBSchemaModel, DomainsListModel, EvaluateInputModel, EvaluateJudgeOutputModel, EvaluateOutputModel, FullEvaluateModel, GoldStandardModel, GoldStandardModelDB, GoldStandardUrlsOutputModel, ParseOutputModel, PostParseInputModel, StatusResponse, WebResourcesModel
 from token_compare import TokenCompare
 import os
-from typing import List,Dict,Tuple, Optional 
+from typing import List,Tuple, Optional 
 import parser_wikipedia as parser_wikipedia
 import parser_nbcnews as parser_nbcnews
 import parser_uefa as parser_uefa
@@ -78,166 +78,6 @@ CUSTOM_PARSERS = {
 }
 
 
-#region MODELLI I/O FASTAPI
-# Modello di risposta per GET /domains
-class DomainsListModel(BaseModel):
-    """    
-        domains: List[str]
-    """
-    domains: List[str]
-
-
-# Modello di risposta per GET /gold_standard
-class GoldStandardModel(BaseModel):
-    """
-        url: str\n
-        domain: str\n
-        title: str\n
-        html_text: str\n
-        gold_text: str
-    """
-    url: str
-    domain: str
-    title: str
-    html_text: str
-    gold_text: str
-
-
-# Modello di risposta per GET /full_gold_standard
-class FullGoldStandardModel(BaseModel):
-    """
-            gold_standard: List[GoldStandardModel]
-    """
-    gold_standard: List[GoldStandardModel]
-
-
-# Modello di risposta per GET /parse
-class ParseOutputModel(BaseModel):
-    """
-        url:str\n
-        domain:str\n
-        title:str\n
-        html_text:str\n
-        parsed_text:str
-    """
-    url:str
-    domain:str
-    title:str
-    html_text:str
-    parsed_text:str
-
-#modello del body nella POST /evaluate
-class EvaluateInputModel(BaseModel):
-    """
-        parsed_text:str\n
-        gold_text:str
-    """
-    parsed_text:str
-    gold_text:str 
-
-#modello di risposta nella POST /evaluate
-class EvaluateOutputModel(BaseModel):
-    """
-        token_level_eval:Dict[str,float]
-
-    """
-    token_level_eval:Dict[str,float]
-
-class PostParseInputModel(BaseModel):
-    """
-        url:str\n
-        local:bool
-    """
-    url:str
-    local:Optional[bool]
-
-#modello di risposta della GET /gold_standard_urls
-class GoldStandardUrlsOutputModel(BaseModel):
-    """gold_standard_urls:List[str]"""
-    gold_standard_urls:List[str]
-
-#modelli di input di POST/add_web_resource e /add_gold_standard
-class AddWebResourceInputModel(BaseModel):
-    """ 
-        url:str\n
-        html_text:str\n
-    """ 
-    url:str
-    html_text:str
-class AddGoldStandardInputModel(BaseModel):
-    """ 
-        url:str\n
-        gold_text:str\n
-    """ 
-    url:str
-    gold_text:str
-#modello di risposta delle POST di inserimento dati (qui sopra)
-class AddOutputModel(BaseModel):
-    """status:str"""
-    status:str
-
-#modello per il web_resources
-class WebResourcesModel(BaseModel):
-    """
-    url:str\n
-    domain:str\n
-    title:str\n
-    html_text:str\n 
-    created_at:str \n
-    """
-    url:str
-    domain:str
-    title:str
-    html_text:str 
-    created_at:str 
-
-#modello per il gold_standard
-class GoldStandardModelDB(BaseModel):
-    """url:str\n
-    gold_text:str\n
-    created_at:str"""
-    url:str
-    gold_text:str
-    created_at:Optional[str] = None
-
-
-#modello di risposta della GET/db_schema
-class DBSchemaModel(BaseModel):
-    """web_resources:WebResourcesModel\n
-    gold_standard:GoldStandardModelDB"""
-    web_resources:WebResourcesModel
-    gold_standard:GoldStandardModelDB
-
-#modello di risposta GET/status
-class StatusResponse(BaseModel):
-    """
-    backend:str\n
-    database:str \n
-    ollama:str 
-    """
-    backend:str
-    database:str 
-    ollama:str 
-
-#modello di risposta di POST/evaluate_judge
-class EvaluateJudgeOutputModel(BaseModel):
-    """
-    model_name:str,\n
-    judge_score:int,\n
-    judge_feedback:str
-    """
-    model_name:str
-    judge_score:int
-    judge_feedback:str
-
-#modello di risposta di GET/full_gs_eval 
-class FullEvaluateModel(BaseModel):
-    """token_level_eval:Dict[str:float]\n
-    judge_score:float"""
-    token_level_eval:Dict[str, float]
-    judge_score:float
-
-#endregion
 
 #region FASTAPI
 
